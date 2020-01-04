30687674808d00506e94bc7be3468fc2a9bb5ad2
Hiago Silva Souza <hiasilva@gmail.com>
2020-01-04 09:01:40 -0300

    Adicionando codigo fonte do projeto

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require("express"),
+        bodyParser = require('body-parser'),
+        uuid = require('uuid/v4'),
+        fs = require('fs'),
+        app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static('public'));
+
+var getGroup = function (code) {
+    if(fs.existsSync(__dirname + '/database/' + code)) {
+        return JSON.parse(fs.readFileSync(__dirname + '/database/' + code))
+    }
+    
+    return false
+};
+
+app.get('/api/grupo/:code/:user', (req, res) => {
+    let group = getGroup(req.params.code)
+    if(group) {
+        group.participantes.forEach(participante => {
+            if(participante.uuid != req.params.user) {
+                participante.amigosecreto = null
+            }
+        });
+        res.send(group)
+    } else {
+        res.send('<script>window.location="/index.html";</script>');
+    }
+});
+
+// cria um grupo
+app.post('/api/grupo', (req, res) => {
+    let userCode = uuid()
+    let groupCode = uuid()
+
+    // enquanto existir um código igual ao gerado, eu gero outro
+    while(fs.existsSync(__dirname + '/database/' + groupCode)) {
+        groupCode = uuid()
+    }
+
+    grupo = {
+        nome: req.body.nome,
+        criado_em: new Date(),
+        uuid: groupCode,
+        finalizado: false,
+        participantes: [{
+            uuid: userCode,
+            is_admin: true,
+            nome: req.body.participante,
+            alterado_em: new Date()
+        }]
+    };
+    fs.writeFileSync(__dirname + '/database/' + groupCode, JSON.stringify(grupo))
+
+    return res.send('<script>window.location="/group.html?code=' + groupCode + '&user='+userCode+'";</script>');
+})
+
+// ingressa em um grupo
+app.post('/api/grupo/:code/ingressar', (req, res) => {
+    let userCode = uuid()
+
+    let group = getGroup(req.params.code)
+    group.participantes.push({
+        uuid: userCode,
+        is_admin: false,
+        nome: req.body.participante,
+        alterado_em: new Date()
+    });
+
+    fs.writeFileSync(__dirname + '/database/' + req.params.code, JSON.stringify(group))
+
+    return res.send('<script>window.location="/group.html?code=' + req.params.code + '&user='+userCode+'";</script>');
+})
+
+// sorteando os nomes
+app.get('/api/grupo/:group/finish/:user', (req, res) => {
+    let group = getGroup(req.params.group);
+
+    // sorteando...
+    let sorteados = [];
+    for(let i = 0; i < group.participantes.length; i++) {
+        // amigo secreto
+        let amigo = Math.floor(Math.random() * group.participantes.length)
+        while(amigo == -1 || amigo == i || sorteados.includes(amigo) || !group.participantes[amigo]) {
+            amigo = Math.floor(Math.random() * group.participantes.length)
+        }
+
+        group.participantes[i].amigosecreto = amigo;
+    }
+
+    group.finalizado = true
+    fs.writeFileSync(__dirname + '/database/' + req.params.group, JSON.stringify(group))
+    return res.send({ok: true});
+})
+
+if(!fs.existsSync(__dirname + '/database'))
+    fs.mkdirSync(__dirname + '/database');
+
+app.listen(8093, function () {
+    console.log('Sistema do amigo secreto inicializado com sucesso.');
+})
---
+++ public/js/group.js
@@ -0,0 +1,81 @@
+$(function () {
+
+    var url = new URL(window.location.href);
+    var code = url.searchParams.get("code");
+    var user = url.searchParams.get("user");
+    if(user) 
+        localStorage.setItem('user_group_' + code, user);
+    else
+        user = localStorage.getItem('user_group_' + code);
+
+    if(!user) {
+        $('#participanteForm').attr('action', '/api/grupo/'+code+'/ingressar')
+        $("#ingressar").show()
+        return;
+    }
+
+    $("#participando").show()
+
+    var isAdmin;
+    var reloadGroup = function () {
+        $('#hasError').hide();
+        if(!isAdmin)
+            $('#adminBar').hide();
+        $.ajax({
+            url: '/api/grupo/' + code + '/' + user,
+            dataType: 'json',
+            success: function (data) {
+                document.title = data.nome
+
+                $('#nomeGrupo').html(data.nome);
+                var lis = '';
+                data.participantes.forEach(item => {
+                    let tags = '';
+                    if(item.uuid == user) {
+                        tags += '(você)';
+
+                        if(item.is_admin) {
+                            isAdmin = true;
+                            $("#adminBar").show();
+                        }
+                    }
+                    if(item.is_admin) {
+                        tags += '(criador)';
+                    }
+                    if(item.uuid == user) {
+                        if(typeof item.amigosecreto == 'number') {
+                            $('#aguardandosorteio').hide();
+                            $('#seuamigo').html('Seu amigo(a) secreto é <strong>'+data.participantes[item.amigosecreto].nome+'</strong><br/><small>Tire um print para não se esquecer hein! =) </small>').show();
+                        }
+                    }
+                    lis += '<li>' + item.nome + ' ' + tags + '</li>';
+                })
+
+                $('#participantes').html(lis)
+
+                if(!data.finalizado) {
+                    setTimeout(reloadGroup, 2000);
+                }
+            },
+            error: function (a,b,c) {
+                console.log(a,b,c)
+                $('#hasError').show();
+                setTimeout(reloadGroup, 2000);
+            }
+        });
+    }
+
+    reloadGroup();
+
+    link = window.location.protocol + '//' + window.location.host + '/group.html?code=' + code;
+    $('#whatslink').attr('href', 'whatsapp://send?text=' + encodeURIComponent('Oi pessoal =) cliquem no link abaixo, coloquem seu nome e aguardem o sorteio para o amigo secreto :P ' + link));
+
+    $('#finishAndSort').click(function () {
+        $.ajax({
+            url: '/api/grupo/' + code + '/finish/' + user,
+            success: function (success) {
+                reloadGroup();
+            }
+        });
+    })
+});
